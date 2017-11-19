7a5bba74538cb7087116c0aba2fe1161c04fc58b
Sudharaka Palamakumbura <sudharakap@mun.ca>
2017-11-18 23:01:32 -0800

    Add support for OAuth authentication with Facebook

--- FrontEndJSFrameworks/rest-server-passport/config.js
+++ FrontEndJSFrameworks/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '797421163774225',
+        clientSecret: 'a3e3d490b96a88b1d743a46bd000a2dc',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
